7f7a1809a221811bc7d5a6672d0fd4fd257e3378
kryptoniteX <89017901+kryptoniteX@users.noreply.github.com>
2021-08-25 12:56:34 +0530

    X01BD: Device have broken dup rules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,6 +22,9 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 # Kernel
 TARGET_KERNEL_CONFIG := X01BD_defconfig
 
+# BrokenHome
+BUILD_BROKEN_DUP_RULES := true
+
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/tpd_gesture"
 
