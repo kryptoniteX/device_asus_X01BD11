ed9ba93dcd232f1d5df66a1304918520ed9f0b3a
Puneet Gautam <89017901+kryptoniteX@users.noreply.github.com>
2021-09-15 08:59:03 +0530

    X01BD: Adaption to Octavi OS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The Hycon OS Project
+# Copyright (C) 2021 The Octavi OS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_X01BD.mk
+    $(LOCAL_DIR)/octavi_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_X01BD-user \
-    aosp_X01BD-userdebug \
-    aosp_X01BD-eng
+    octavi_X01BD-user \
+    octavi_X01BD-userdebug \
+    octavi_X01BD-eng
--- octavi_X01BD.mk
+++ octavi_X01BD.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2021 The Octavi OS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common Superior stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -19,7 +19,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_X01BD
+PRODUCT_NAME := octavi_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS Max Pro M2
